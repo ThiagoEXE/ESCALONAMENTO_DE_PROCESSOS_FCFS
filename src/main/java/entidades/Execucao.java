dd599193d2d2614ae2ab8d8bbe5d47d8d09cb88f
ThiagoEXE <36972689+ThiagoEXE@users.noreply.github.com>
2019-09-16 21:52:57 -0300

    Initial commit

---
+++ src/main/java/entidades/Execucao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Thiago
+ */
+public class Execucao {
+    public static void main(String[] args) {
+       
+        Fcfs fcfs = new Fcfs(); 
+        fcfs.Fcfs();
+    }
+    
+}
